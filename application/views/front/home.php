00100e04092f48178d192bbfbaa943ce568fb9bd
Muqaffi Gusticahyo <muqaffigusticahyo14@gmail.com>
2017-11-26 13:17:07 +0700

    prototype

---
+++ application/views/front/home.php
@@ -0,0 +1,205 @@
+
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row1" style="z-index: 100; position: fixed;">
+  <header id="header" class="hoc clear" > 
+    <!-- ################################################################################################ -->
+    <div id="logo" class="fl_left">
+      <h1><a href="index.html">MonoCyber</a></h1>
+    </div>
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a  href="#">Monitor</a>
+         <!--  <ul>
+            <li><a href="pages/gallery.html">Gallery</a></li>
+            <li><a href="pages/full-width.html">Full Width</a></li>
+            <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>
+            <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
+            <li><a href="pages/basic-grid.html">Basic Grid</a></li>
+          </ul> -->
+        </li>
+        <!-- <li><a class="drop" href="#">Gallery</a>
+          <ul>
+            <li><a href="#">Level 2</a></li>
+            <li><a class="drop" href="#">Level 2 + Drop</a>
+              <ul>
+                <li><a href="#">Level 3</a></li>
+                <li><a href="#">Level 3</a></li>
+                <li><a href="#">Level 3</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Level 2</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Order</a></li>
+        <li><a href="#">Contact</a></li> -->
+      </ul>
+    </nav>
+    <!-- ################################################################################################ -->
+  </header>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper bgded overlay" id="backgroundImg">
+  <div id="pageintro" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <article>
+      <p>Moto</p>
+    
+<blockquote style="font-size: 20px;">Listrik untuk Kehidupan yang Lebih Baik</blockquote>
+
+      <p></p>
+      <footer><a class="btn" href="#">Order</a></footer>
+    </article>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle">
+      <h6 class="heading">Our Team</h6>
+      <p></p>
+    </div>
+    <ul class="nospace group services">
+     <li class="one_third first">
+        <article class="infobox" >
+          <h6 class="heading">Job : </h6>
+          <h6>Name : </h6>
+          <h6>address : </h6>
+          <h6>old : </h6>
+          <h6>motivation : </h6>
+        <img style="width: auto; height: auto;" src="<?=base_url();?>assets/img/background/Background_Box2.jpg">
+        </article>
+      </li>
+      <li class="one_third">
+        <article class="infobox">
+          <h6 class="heading">Job : </h6>
+          <h6>Name : </h6>
+          <h6>address : </h6>
+          <h6>old : </h6>
+          <h6>motivation : </h6>
+          <img style="width: auto; height: auto;" src="<?=base_url();?>assets/img/background/Background_Buket.jpg">
+        </article>
+      </li>
+      <li class="one_third">
+        <article class="infobox">
+          <h6 class="heading">Job : </h6>
+          <h6>Name : </h6>
+          <h6>address : </h6>
+          <h6>old : </h6>
+          <h6>motivation : </h6>
+          <img style="width: auto; height: auto;" src="<?=base_url();?>assets/img/background/Background_FC.jpg">
+        </article>
+      </li>
+      <li class="one_third first">
+        <article class="infobox">
+          <h6 class="heading">Job : </h6>
+          <h6>Name : </h6>
+          <h6>address : </h6>
+          <h6>old : </h6>
+          <h6>motivation : </h6>
+          <img style="width: auto; height: auto;"  src="<?=base_url();?>assets/img/background/Background_SF.jpg">
+        </article>
+      </li>
+    </ul>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- <div class="wrapper bgded overlay" id="backgroundImg1">
+  <div class="hoc container clear"> 
+    ################################################################################################
+    
+    <!-- ################################################################################################ -->
+  <!-- </div>
+</div> -->
+<div class="wrapper bgded overlay" id="backgroundImg1">
+  <div id="pageintro" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <section class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle">
+      <h6 class="heading">Volutpat mollis vivamus</h6>
+      <p>Pharetra libero non facilisis imperdiet mi augue feugiat.</p>
+    </div>
+    <ul class="nospace group">
+      <li class="one_half first">
+        <article class="group">
+          <div class="one_half first"><a href="#"><img src="images/demo/320x240.png" alt=""></a></div>
+          <div class="one_half">
+            <h3 class="heading font-x1">Amet sodales tellus</h3>
+            <p>Nisl sit amet mollis enim velit posuere metus nam tempus odio in erat integer ante risus tempor quis ultrices quis [&hellip;]</p>
+            <footer><a href="#">Read More &raquo;</a></footer>
+          </div>
+        </article>
+      </li>
+      <li class="one_half">
+        <article class="group">
+          <div class="one_half first"><a href="#"><img src="images/demo/320x240.png" alt=""></a></div>
+          <div class="one_half">
+            <h3 class="heading font-x1">Fringilla non augue</h3>
+            <p>Et turpis morbi mi neque facilisis sollicitudin faucibus ac primis in faucibus orci luctus et ultrices posuere cubilia [&hellip;]</p>
+            <footer><a href="#">Read More &raquo;</a></footer>
+          </div>
+        </article>
+      </li>
+    </ul>
+    <!-- ################################################################################################ -->
+    <div class="clear"></div>
+  </section>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row4">
+  <footer id="footer" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <h6 class="heading">Lalapeden</h6>
+    <p>Vivamus quis tellus eget quam elementum aliquam cras nibh mi adipiscing a sodales in tincidunt ut enim praesent tempor molestie metus ut pretium odio.</p>
+    <ul class="faico clear">
+      <li><a class="faicon-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+      <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+      <li><a class="faicon-dribble" href="#"><i class="fa fa-dribbble"></i></a></li>
+      <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+      <li><a class="faicon-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+      <li><a class="faicon-vk" href="#"><i class="fa fa-vk"></i></a></li>
+    </ul>
+    <nav>
+      <ul class="nospace">
+        <li><a href="#"><i class="fa fa-lg fa-home"></i></a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Privacy</a></li>
+        <li><a href="#">Disclaimer</a></li>
+        <li><a href="#">Cookies</a></li>
+      </ul>
+    </nav>
+    <!-- ################################################################################################ -->
+  </footer>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
