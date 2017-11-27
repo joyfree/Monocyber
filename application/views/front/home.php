eae6e730e2d08d5ab56a3ff2cf536d587938c3a8
Muqaffi Gusticahyo <muqaffigusticahyo14@gmail.com>
2017-11-27 14:06:43 +0700

    visi misi

--- application/views/front/home.php
+++ application/views/front/home.php
@@ -1,18 +1,18 @@
 
 <body id="top">
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<div class="wrapper row1" style="z-index: 100; position: fixed;">
-  <header id="header" class="hoc clear" > 
-    <!-- ################################################################################################ -->
-    <div id="logo" class="fl_left">
-      <h1><a href="index.html">MonoCyber</a></h1>
-    </div>
-    <nav id="mainav" class="fl_right">
-      <ul class="clear">
-        <li class="active"><a href="index.html">Home</a></li>
-        <li><a  href="#">Monitor</a>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div class="wrapper row1" style="z-index: 100; position: fixed;">
+    <header id="header" class="hoc clear" > 
+      <!-- ################################################################################################ -->
+      <div id="logo" class="fl_left">
+        <h1><a href="index.html">MonoCyber</a></h1>
+      </div>
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li class="active"><a href="index.html">Home</a></li>
+          <li><a  href="#">Monitor</a>
          <!--  <ul>
             <li><a href="pages/gallery.html">Gallery</a></li>
             <li><a href="pages/full-width.html">Full Width</a></li>
@@ -49,11 +49,11 @@
     <!-- ################################################################################################ -->
     <article>
       <p>Moto</p>
-    
-<blockquote style="font-size: 20px;">Listrik untuk Kehidupan yang Lebih Baik</blockquote>
+
+      <blockquote style="font-size: 20px;">Listrik untuk Kehidupan yang Lebih Baik</blockquote>
 
       <p></p>
-      <footer><a class="btn" href="#">Order</a></footer>
+      <footer><a class="btn" href="#">Monitor</a></footer>
     </article>
     <!-- ################################################################################################ -->
   </div>
@@ -71,50 +71,50 @@
     </div>
     <ul class="nospace group services">
      <li class="one_third first">
-        <article class="infobox" >
-          <h6 class="heading">Job : </h6>
-          <h6>Name : </h6>
-          <h6>address : </h6>
-          <h6>old : </h6>
-          <h6>motivation : </h6>
+      <article class="infobox" >
+        <h6 class="heading">Job : </h6>
+        <h6>Name : </h6>
+        <h6>address : </h6>
+        <h6>old : </h6>
+        <h6>motivation : </h6>
         <img style="width: auto; height: auto;" src="<?=base_url();?>assets/img/background/Background_Box2.jpg">
-        </article>
-      </li>
-      <li class="one_third">
-        <article class="infobox">
-          <h6 class="heading">Job : </h6>
-          <h6>Name : </h6>
-          <h6>address : </h6>
-          <h6>old : </h6>
-          <h6>motivation : </h6>
-          <img style="width: auto; height: auto;" src="<?=base_url();?>assets/img/background/Background_Buket.jpg">
-        </article>
-      </li>
-      <li class="one_third">
-        <article class="infobox">
-          <h6 class="heading">Job : </h6>
-          <h6>Name : </h6>
-          <h6>address : </h6>
-          <h6>old : </h6>
-          <h6>motivation : </h6>
-          <img style="width: auto; height: auto;" src="<?=base_url();?>assets/img/background/Background_FC.jpg">
-        </article>
-      </li>
-      <li class="one_third first">
-        <article class="infobox">
-          <h6 class="heading">Job : </h6>
-          <h6>Name : </h6>
-          <h6>address : </h6>
-          <h6>old : </h6>
-          <h6>motivation : </h6>
-          <img style="width: auto; height: auto;"  src="<?=base_url();?>assets/img/background/Background_SF.jpg">
-        </article>
-      </li>
-    </ul>
-    <!-- ################################################################################################ -->
-    <!-- / main body -->
-    <div class="clear"></div>
-  </main>
+      </article>
+    </li>
+    <li class="one_third">
+      <article class="infobox">
+        <h6 class="heading">Job : </h6>
+        <h6>Name : </h6>
+        <h6>address : </h6>
+        <h6>old : </h6>
+        <h6>motivation : </h6>
+        <img style="width: auto; height: auto;" src="<?=base_url();?>assets/img/background/Background_Buket.jpg">
+      </article>
+    </li>
+    <li class="one_third">
+      <article class="infobox">
+        <h6 class="heading">Job : </h6>
+        <h6>Name : </h6>
+        <h6>address : </h6>
+        <h6>old : </h6>
+        <h6>motivation : </h6>
+        <img style="width: auto; height: auto;" src="<?=base_url();?>assets/img/background/Background_FC.jpg">
+      </article>
+    </li>
+    <li class="one_third first">
+      <article class="infobox">
+        <h6 class="heading">Job : </h6>
+        <h6>Name : </h6>
+        <h6>address : </h6>
+        <h6>old : </h6>
+        <h6>motivation : </h6>
+        <img style="width: auto; height: auto;"  src="<?=base_url();?>assets/img/background/Background_SF.jpg">
+      </article>
+    </li>
+  </ul>
+  <!-- ################################################################################################ -->
+  <!-- / main body -->
+  <div class="clear"></div>
+</main>
 </div>
 <!-- ################################################################################################ -->
 <!-- ################################################################################################ -->
@@ -141,65 +141,31 @@
 <div class="wrapper row3">
   <section class="hoc container clear"> 
     <!-- ################################################################################################ -->
-    <div class="sectiontitle">
-      <h6 class="heading">Volutpat mollis vivamus</h6>
-      <p>Pharetra libero non facilisis imperdiet mi augue feugiat.</p>
-    </div>
     <ul class="nospace group">
       <li class="one_half first">
         <article class="group">
-          <div class="one_half first"><a href="#"><img src="images/demo/320x240.png" alt=""></a></div>
           <div class="one_half">
-            <h3 class="heading font-x1">Amet sodales tellus</h3>
-            <p>Nisl sit amet mollis enim velit posuere metus nam tempus odio in erat integer ante risus tempor quis ultrices quis [&hellip;]</p>
-            <footer><a href="#">Read More &raquo;</a></footer>
-          </div>
-        </article>
-      </li>
-      <li class="one_half">
-        <article class="group">
-          <div class="one_half first"><a href="#"><img src="images/demo/320x240.png" alt=""></a></div>
-          <div class="one_half">
-            <h3 class="heading font-x1">Fringilla non augue</h3>
-            <p>Et turpis morbi mi neque facilisis sollicitudin faucibus ac primis in faucibus orci luctus et ultrices posuere cubilia [&hellip;]</p>
-            <footer><a href="#">Read More &raquo;</a></footer>
-          </div>
-        </article>
-      </li>
-    </ul>
-    <!-- ################################################################################################ -->
-    <div class="clear"></div>
-  </section>
-</div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<div class="wrapper row4">
-  <footer id="footer" class="hoc clear"> 
-    <!-- ################################################################################################ -->
-    <h6 class="heading">Lalapeden</h6>
-    <p>Vivamus quis tellus eget quam elementum aliquam cras nibh mi adipiscing a sodales in tincidunt ut enim praesent tempor molestie metus ut pretium odio.</p>
-    <ul class="faico clear">
-      <li><a class="faicon-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-      <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-      <li><a class="faicon-dribble" href="#"><i class="fa fa-dribbble"></i></a></li>
-      <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-      <li><a class="faicon-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
-      <li><a class="faicon-vk" href="#"><i class="fa fa-vk"></i></a></li>
-    </ul>
-    <nav>
-      <ul class="nospace">
-        <li><a href="#"><i class="fa fa-lg fa-home"></i></a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">Privacy</a></li>
-        <li><a href="#">Disclaimer</a></li>
-        <li><a href="#">Cookies</a></li>
+            <h3 class="heading font-x1">Misi</h3>
+            <p>Menjalankan bisnis kelistrikan dan bidang lain yang terkait, berorientasi pada kepuasan pelanggan, anggota perusahaan dan pemegang saham. <br>
+              Menjadikan tenaga listrik sebagai media untuk meningkatkan kualitas kehidupan masyarakat.<br>
+              Mengupayakan agar tenaga listrik menjadi pendorong kegiatan ekonomi.<br>
+              Menjalankan kegiatan usaha yang berwawasan lingkungan.</p>
+
+            </div>
+          </article>
+        </li>
+        <li class="one_half">
+          <article class="group">
+            <div class="one_half first"><a href="#"><img src="images/demo/320x240.png" alt=""></a></div>
+            <div class="one_half">
+              <h3 class="heading font-x1">Visi</h3>
+              <p>Diakui sebagai Perusahaan Kelas Dunia yang Bertumbuh kembang, Unggul dan terpercaya dengan bertumpu pada Potensi Insani.</p>
+              
+            </div>
+          </article>
+        </li>
       </ul>
-    </nav>
-    <!-- ################################################################################################ -->
-  </footer>
-</div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
+      <!-- ################################################################################################ -->
+      <div class="clear"></div>
+    </section>
+  </div>
