694fdef2f4e67c11a24f086f98a01946dd745c7d
Muqaffi Gusticahyo <muqaffigusticahyo14@gmail.com>
2017-12-27 01:17:12 +0700

    edit karyawan note: check halaman utama disik :v

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -7,6 +7,7 @@ class Admin extends CI_Controller
 		parent::__construct();
 		$this->load->model('Monitor_model');
 		$this->load->model('Model_Admin');
+		$this->load->library('upload');
 	}
 	public function index(){
 		if($this->session->userdata('logged_in') == true){
@@ -44,9 +45,12 @@ class Admin extends CI_Controller
 		$this->session->set_userdata($account);
 		redirect('Admin','refresh');
 	}
-	public function Beranda($link=null){
+	public function Beranda($link=null,$id=null){
 
 		if($this->session->userdata('logged_in') == true){
+			if($id != null){
+				$data['Data'] = $this->Model_Admin->GetDataKaryawan($id);
+			}
 			$data['karyawan'] = $this->Model_Admin->Getkaryawan();
 			$data['visimisi'] = $this->Model_Admin->GetVisiMisi();
 			if($link == null){
@@ -63,7 +67,19 @@ class Admin extends CI_Controller
 	}
 	public function add_karyawan(){
 		if($this->session->userdata('logged_in') == true){
-			$data = $this->input->post();
+			$this->load->library('upload');
+			$data['nama'] = $this->input->post('nama');
+			$data['nip'] = $this->input->post('nip');
+			$data['jabatan'] = $this->input->post('jabatan');
+			$data['tanggal_lahir'] = $this->input->post('tanggal_lahir');
+			if($_FILES['foto']['name'] != ''){
+				$this->do_upload('foto');
+				$filename = $this->upload->data();
+				$data['foto'] = $filename['file_name'];
+			}
+			// $data['foto'] = $_FILES['foto'];
+			// print_r($_FILES['foto']);
+			
 			if($this->Model_Admin->add_karyawan($data)){
 				$this->session->set_flashdata('flash', 'Data telah di tambahkan');	
 			}else{
@@ -76,6 +92,60 @@ class Admin extends CI_Controller
 		}
 
 
+	}
+
+	public function edit_karyawan(){
+		if($this->session->userdata('logged_in') == true){
+			$this->load->library('upload');
+			$data['no'] = $this->input->post('no');
+			$data['nama'] = $this->input->post('nama');
+			$data['nip'] = $this->input->post('nip');
+			$data['jabatan'] = $this->input->post('jabatan');
+			$data['tanggal_lahir'] = $this->input->post('tanggal_lahir');
+			if($_FILES['foto']['name'] != ''){
+				$this->do_upload('foto');
+				$filename = $this->upload->data();
+				$data['foto'] = $filename['file_name'];
+			}
+			// $data['foto'] = $_FILES['foto'];
+			// print_r($_FILES['foto']);
+			
+			if($this->Model_Admin->edit_karyawan($data)){
+				$this->session->set_flashdata('flash', 'Data telah di ubah');	
+			}else{
+				$this->session->set_flashdata('flash', 'Data gagal di ubah');	
+			}
+			redirect('Admin','refresh');	
+		}else{
+			$data['content'] = 'admin/admin-content/login';
+			$this->load->view('admin/template_login',$data);
+		}
+	}
+
+	public function do_upload($nama)
+	{
+		$time = time();
+		$date = date('y-m-d-h-i-s',$time);
+		$ext = pathinfo($nama, PATHINFO_EXTENSION);
+		$config['upload_path']          = './assets/img/';
+		$config['file_name']            =  'karyawan_'.$date.''.$ext;
+		$config['allowed_types']        = 'gif|jpg|png';
+		$config['max_size']             = 1000000;
+		$config['max_width']            = 102400;
+		$config['max_height']           = 768000;
+
+
+		$this->load->library('upload', $config);
+		$this->upload->initialize($config);
+		if ( ! $this->upload->do_upload('foto'))
+		{
+			$error = array('error' => $this->upload->display_errors());
+
+			$this->session->set_flashdata('flash', 'Data Foto tidak sesuai');	
+			redirect('admin/form_karyawan','refresh');	
+		}
+		
+		return $this->upload->data();
 	}
 	public function delete_karyawan(){
 		if($this->session->userdata('logged_in') == true){
